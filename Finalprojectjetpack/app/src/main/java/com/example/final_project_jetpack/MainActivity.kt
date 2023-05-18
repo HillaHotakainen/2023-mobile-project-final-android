8bfcd7b6502bf1ded24e27296e3bdb37fa28862a
Hilla Hotakainen <hilla.hotakainen@outlook.com>
2023-05-18 15:47:48 +0300

    fetch all behind a button

--- Finalprojectjetpack/app/src/main/java/com/example/final_project_jetpack/MainActivity.kt
+++ Finalprojectjetpack/app/src/main/java/com/example/final_project_jetpack/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.final_project_jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.final_project_jetpack.ui.theme.FinalprojectjetpackTheme
@@ -21,7 +26,8 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 
 class MainActivity : ComponentActivity() {
-    private val users = mutableListOf<User>()
+    private val users = mutableStateListOf<User>()
+    private var isLoading = mutableStateOf(false)
 
     data class User(
         val firstName: String,
@@ -30,10 +36,17 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fetchUsers()
+        setContent {
+            FinalprojectjetpackTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    UserList(users = users, isLoading = isLoading.value, fetchUsers = this::fetchUsers)
+                }
+            }
+        }
     }
 
     private fun fetchUsers() {
+        isLoading.value = true
         val url = "https://dummyjson.com/users"
         val requestQueue = Volley.newRequestQueue(this)
 
@@ -49,12 +62,11 @@ class MainActivity : ComponentActivity() {
                     )
                     users.add(user)
                 }
-                setContent {
-                    UserList(users = users)
-                }
+                isLoading.value = false
             },
             { error ->
                 // Handle error
+                isLoading.value = false
             }
         )
 
@@ -68,14 +80,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun UserList(users: List<MainActivity.User>) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        LazyColumn {
-            items(users) { user ->
-                Text(text = "${user.firstName} ${user.lastName}")
+fun UserList(users: List<MainActivity.User>, isLoading: Boolean, fetchUsers: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Button(
+            onClick = { fetchUsers() },
+            enabled = !isLoading && users.isEmpty() // Disable the button if users are already fetched or currently loading
+        ) {
+            Text(text = "Fetch Users")
+        }
+        if (isLoading) {
+            Text(text = "Loading...")
+        } else {
+            if (users.isNotEmpty()) {
+                Surface(modifier = Modifier.weight(1f)) {
+                    LazyColumn {
+                        items(users) { user ->
+                            Text(text = "${user.firstName} ${user.lastName}")
+                        }
+                    }
+                }
+            } else {
+                Text(text = "No users available")
             }
         }
     }
